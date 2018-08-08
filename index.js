60315e51bf0ae563ee1f7e9b3dc57cf66da65e5a
Aviwe Ngqukumba <aviwe3226@gmail.com>
2018-08-08 11:04:18 +0200

    change landing page login form to link to days route

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (process.env.DATABASE_URL) {
 
 const connectionString =
   process.env.DATABASE_URL ||
-  "postgresql://postgres:lavish@localhost:5432/waiter-availability";
+  "postgresql://aviwe:aviwe@localhost:5432/waiter-availability";
 
 const pool = new Pool({
   connectionString,
